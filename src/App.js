dd8a2cb58abca26305568b29712fe3108867de90
tanuha <tanuha@zeoalliance.com>
2019-02-23 07:22:49 +0200

    # base config

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        Mate hackathon
+      </div>
+    );
+  }
+}
+
+export default App;
