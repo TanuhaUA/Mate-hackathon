33487012d621bb0ec18c3b463a0c7d3d59c7fead
YuraSadilenko <yura.sadilenko.it@gmail.com>
2019-02-23 16:44:30 +0200

    commit

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.scss';
-import '../../Styles/main.scss'
+import '../../Styles/main.scss';
 
 class HomePage extends React.Component {
   render() {
@@ -9,8 +9,25 @@ class HomePage extends React.Component {
         {/* <Logo color='#676767' fontSize='30px'/> */}
         <h1 className="home__title home__title--main">Fusce rutrum pretium lorem sempter nulla</h1>
         <p className="home__title home__title--sub"> lorem ipsum furcum pretium lorem septer nulla gu op dunap sulem lorem do burta</p>
-        <div className="home__icon">
-          <p className="home__icon-text">flndslfn</p> 
+        <div className="home__icon-box">
+          <div className="home__icon">
+            <p className="home__icon-text">About</p> 
+          </div>
+          <div className="home__icon">
+            <p className="home__icon-text">Team</p> 
+          </div>
+          <div className="home__icon">
+            <p className="home__icon-text">Services</p> 
+          </div>
+          <div className="home__icon">
+            <p className="home__icon-text">Portfolio</p> 
+          </div>
+          <div className="home__icon">
+            <p className="home__icon-text">Blog</p> 
+          </div>
+          <div className="home__icon">
+            <p className="home__icon-text">Contacs</p> 
+          </div>
         </div>
       </div>
     )
