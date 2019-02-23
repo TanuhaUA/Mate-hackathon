07d803fca05dae28eb5501d7aba89d8f936aaf9a
ivankvitka <ivan.kvitka@gmail.com>
2019-02-23 12:30:08 +0200

    add main title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Index from "./Components/OurSkills/index";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Mate hackathon
+        <Index />
       </div>
     );
   }
