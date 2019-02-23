d3e53a5651db1a0ffbd2bd3ad3605841f14cdda8
ivankvitka <ivan.kvitka@gmail.com>
2019-02-23 16:53:35 +0200

    add our skills adaptive section

--- src/Components/OurSkills/index.js
+++ src/Components/OurSkills/index.js
@@ -1,16 +1,37 @@
 import React, {Component} from 'react';
 import './style.scss';
-import Title from './../Title'
+import Title from './../Title';
+import Progress from './Progress';
 
 class OurSkills extends Component {
-  // constructor() {
-  //   super();
-  // }
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+    this.getProgressValue = this.getProgressValue.bind(this);
+  }
+
+
+  getProgressValue() {
+    return this.state.data.map(item => <Progress key={item.value} skill={item.skill} value={item.value}/>)
+  }
+
+  componentDidMount() {
+    fetch('https://tanuhaua.github.io/datas-file-json/bhagaskara/skills.json')
+      .then(response => response.json()).then(response => this.setState({data: response}));
+  }
+
 
   render() {
+    const title = 'Our';
+    const titlePurple = 'skills';
     return (
       <div className="our-skills">
-      <Title />
+        <Title title={title} titlePurple={titlePurple}/>
+        <div className="our-skills__levels">
+          {this.getProgressValue()}
+        </div>
       </div>
     )
   }
