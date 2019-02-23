666ae55e1f22b48d039dc4d3b4baab7b9f5e2c6d
Ali-X <alina240895@yandex.ru>
2019-02-23 15:56:09 +0200

    Implement Routing component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Styles/main.scss'
-import OurSkills from "./Components/OurSkills";
+import Header from "./Components/Header";
+import Routing from "./Components/Routing";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <OurSkills />
+        <Header/>
+        <Routing/>
       </div>
     );
   }
