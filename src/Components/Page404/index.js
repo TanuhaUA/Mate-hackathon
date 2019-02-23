4b69dba1a136f90c122fdbd9dc93465c13334621
Ali-X <alina240895@yandex.ru>
2019-02-23 15:54:55 +0200

    Implement Page404 component

---
+++ src/Components/Page404/index.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+import './style.scss';
+
+const Page404 = () => (
+  <section className="error-container">
+    <div className='error-container__title'>
+      <div className='title'>Page is not found!</div>
+    </div>
+    <span>4</span>
+    <span><span className="screen-reader-text">0</span></span>
+    <span>4</span>
+  </section>
+);
+
+export default Page404;
