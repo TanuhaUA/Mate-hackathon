a9af710ba0afdcdfa89422080f75bb4e056e586f
YuraSadilenko <yura.sadilenko.it@gmail.com>
2019-02-23 14:35:13 +0200

    commit fixes to bg

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import HomePage from './Components/HomePage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         Mate hackathon
+        <HomePage/>
       </div>
     );
   }
---
+++ src/Components/HomePage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.scss';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="home">
+
+      </div>
+    )
+  }
+}
+
+export default HomePage;
