45a9b6898817d72056209da2ded914f03b299ce7
YuraSadilenko <yura.sadilenko.it@gmail.com>
2019-02-23 16:25:39 +0200

    add icons

--- src/Components/HomePage/index.js
+++ src/Components/HomePage/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './style.scss';
+import '../../Styles/main.scss'
 
 class HomePage extends React.Component {
   render() {
     return (
       <div className="home">
-
+        {/* <Logo color='#676767' fontSize='30px'/> */}
+        <h1 className="home__title home__title--main">Fusce rutrum pretium lorem sempter nulla</h1>
+        <p className="home__title home__title--sub"> lorem ipsum furcum pretium lorem septer nulla gu op dunap sulem lorem do burta</p>
+        <div className="home__icon">
+          <p className="home__icon-text">flndslfn</p> 
+        </div>
       </div>
     )
   }
