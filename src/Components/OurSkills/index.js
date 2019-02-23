e3dcef92345f9d2049b0d90bdd8248108ed0fc6c
ivankvitka <ivan.kvitka@gmail.com>
2019-02-23 13:29:23 +0200

    add main title

---
+++ src/Components/OurSkills/index.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import './style.scss';
+import Title from './../Title'
+
+class Index extends Component {
+  // constructor() {
+  //   super();
+  // }
+
+  render() {
+    return (
+      <div className="our-skills">
+      <Title />
+      </div>
+    )
+  }
+}
+
+export default Index
