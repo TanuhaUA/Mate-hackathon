bfaaeecac3d9fe2eb2912169b486a093132bd4d6
Veronika Serhiienko <veronika.serhiienko1@gmail.com>
2019-02-24 19:59:58 +0200

    make build

---
+++ build/precache-manifest.bf7245b4f1617a8ca0e08ed99a8d74cb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8536ed1da0daf7476ccd01951a6eb0b1",
+    "url": "/static/media/home.8536ed1d.jpg"
+  },
+  {
+    "revision": "8ef39e783bb6546ab9411b58e7300f6d",
+    "url": "/static/media/Layer4.8ef39e78.jpg"
+  },
+  {
+    "revision": "90e1d3559ac52f7f0f77a86e1bfd632d",
+    "url": "/static/media/Lato.90e1d355.ttf"
+  },
+  {
+    "revision": "c3be8cbba61a6e6d883ba7d3b404f519",
+    "url": "/static/media/HelveticaNeueBold.c3be8cbb.ttf"
+  },
+  {
+    "revision": "31d1ca77eca12c27cb7d61666db19be1",
+    "url": "/static/media/Existence-Light.31d1ca77.otf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "1d6aefeaa711647ae52e",
+    "url": "/static/js/main.1d6aefea.chunk.js"
+  },
+  {
+    "revision": "a66add37c75d96f43f08",
+    "url": "/static/js/2.a66add37.chunk.js"
+  },
+  {
+    "revision": "1d6aefeaa711647ae52e",
+    "url": "/static/css/main.8e783b3f.chunk.css"
+  },
+  {
+    "revision": "e9c78cb3f5e541538818515d42b98e4b",
+    "url": "/index.html"
+  }
+];
